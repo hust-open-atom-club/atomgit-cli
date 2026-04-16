a866e42634d301d6b6a7d1ad47c6dfc91a288d8e
xiaogang <xiaogang@csdn.net>
2026-04-16 16:12:26 +0800

    升级版本号

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ REPO_OWNER="${AG_REPO_OWNER:-GitCode}"
 REPO_NAME="${AG_REPO_NAME:-ag-cli}"
 BASE_URL="https://gitcode.com/${REPO_OWNER}/${REPO_NAME}"
 # 默认下载该 tag 下的预编译包。仓库内请随最新 Release 更新；也可用 scripts/build-release.sh 生成 dist/<tag>/install.sh（已写入本次 TAG）再上传。
-_BUNDLED_TAG="v0.2"
+_BUNDLED_TAG="v0.3"
 DEFAULT_VERSION="${AG_DEFAULT_VERSION:-${_BUNDLED_TAG}}"
 VERSION="${AG_VERSION:-$DEFAULT_VERSION}"
 
