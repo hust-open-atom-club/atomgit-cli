288e59d9caaa5cbc5b88476e25c595928a759948
Mr. why <Mr-why@foxmail.com>
2026-08-11 14:14:01 +0800

    fix: pin nixpkgs inputs to NJU mirror

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "Development environment for the AtomGit CLI";
 
-  inputs.nixpkgs.url = "git+https://mirrors.cernet.edu.cn/nixpkgs.git?ref=nixos-unstable&shallow=1";
-  inputs.nixpkgs-darwin.url = "git+https://mirrors.cernet.edu.cn/nixpkgs.git?ref=nixpkgs-26.05-darwin&shallow=1";
+  inputs.nixpkgs.url = "git+https://mirror.nju.edu.cn/git/nixpkgs.git?ref=nixos-unstable&shallow=1";
+  inputs.nixpkgs-darwin.url = "git+https://mirror.nju.edu.cn/git/nixpkgs.git?ref=nixpkgs-26.05-darwin&shallow=1";
 
   outputs = { self, nixpkgs, nixpkgs-darwin, ... }:
     let
