996fd25284d2ff7c53c56bc572a28e3506508fa2
mudongliang <mudongliangabcd@gmail.com>
2026-09-05 17:52:04 +0800

    chore: update Nix packages

--- nix/stable.nix
+++ nix/stable.nix
@@ -4,9 +4,9 @@
 }:
 
 let
-  version = "0.7.2";
-  commit = "fd2d0c29349e2251732711e877a1a58fabbeec54";
-  buildDate = "2026-08-19T00:13:48+08:00";
+  version = "0.7.3";
+  commit = "6cfffd1f9ffc8e240baff316031f89835cb013e8";
+  buildDate = "2026-09-05T17:05:40+08:00";
 in
 pkgs.buildGoModule (
   mkAg {
@@ -18,7 +18,7 @@ pkgs.buildGoModule (
     vendorHash = "sha256-lvnlTenDlg5pTJghBZRRy8J5vig1IW9iEISN93cnRfs=";
     src = pkgs.fetchzip {
       url = "https://raw.atomgit.com/hust-open-atom-club/atomgit-cli/archive/refs/heads/v${version}.tar.gz";
-      hash = "sha256-E1T093LkccgLPPNs5OokxY5tw4HEMmRy+ulaROnuSCE=";
+      hash = "sha256-Yvv65+tNO9JJ4aW0KGDbjYzXAEWp9QFzCrrYMwdSl8M=";
     };
   }
 )
